61930846db515a34182069ad816cfaad0f3cfe33
FlugsCoding <41925531+FlugsCoding@users.noreply.github.com>
2020-04-22 12:00:58 +0100

    info & routing fixes
    edited some information about jobs and fixed the routing error

---
+++ build/precache-manifest.f531d566c761e45b47870068fccc2788.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea966970ff0cebcf0e1ae225a3fec9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e9ec3162a1b937aec45",
+    "url": "./static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a50d0473d56aac5f9731",
+    "url": "./static/css/main.4eb8f961.chunk.css"
+  },
+  {
+    "revision": "0e9ec3162a1b937aec45",
+    "url": "./static/js/2.03736bf9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.03736bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50d0473d56aac5f9731",
+    "url": "./static/js/main.f1dba242.chunk.js"
+  },
+  {
+    "revision": "e6b0745271ec3f98a57c",
+    "url": "./static/js/runtime-main.71e00d54.js"
+  },
+  {
+    "revision": "3d54aa3adaa1ae641eed2f3ef3ec895a",
+    "url": "./static/media/bg.3d54aa3a.jpg"
+  },
+  {
+    "revision": "01e5c238be34e8ac8a3c430704bd5d14",
+    "url": "./static/media/ddmap.01e5c238.jpg"
+  },
+  {
+    "revision": "10ffb503011682e5bdbc145ad5e2e309",
+    "url": "./static/media/delivery.10ffb503.jpg"
+  },
+  {
+    "revision": "023a89a1af2d52ea9396008172250478",
+    "url": "./static/media/fire.023a89a1.jpg"
+  },
+  {
+    "revision": "c619f6b024d390f2b3aaa48258d52e09",
+    "url": "./static/media/fullmap.c619f6b0.jpg"
+  },
+  {
+    "revision": "b17111465ce39a8b4964bf619bdc8397",
+    "url": "./static/media/intromap.b1711146.jpg"
+  },
+  {
+    "revision": "655609e4fc70207a0b85c0eab8a51561",
+    "url": "./static/media/jobsmap.655609e4.jpg"
+  },
+  {
+    "revision": "d39f5a73f62472329eba4b0566f93457",
+    "url": "./static/media/logo.d39f5a73.png"
+  },
+  {
+    "revision": "eed948e6a719a158b25d3ca45a69ff2c",
+    "url": "./static/media/medic.eed948e6.jpg"
+  },
+  {
+    "revision": "dc5e0d853277154b594d66d91edd4b66",
+    "url": "./static/media/police.dc5e0d85.jpg"
+  },
+  {
+    "revision": "71eda4049e82d9c017a6a06b9bbc6ad1",
+    "url": "./static/media/rc.71eda404.jpg"
+  },
+  {
+    "revision": "2bae63da30cc49125f92e70d642f605b",
+    "url": "./static/media/ss.2bae63da.jpg"
+  },
+  {
+    "revision": "648e15142b9f8f088bca5b5d7a54fdb0",
+    "url": "./static/media/taxi.648e1514.jpg"
+  }
+]);
